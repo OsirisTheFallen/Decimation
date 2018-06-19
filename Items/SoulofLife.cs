97385feab048c7fe54d6897b9459cf6e68a420ab
William <fylozdev@gmail.com>
2018-06-19 19:56:15 -0400

    Add files via upload

---
+++ Items/SoulofLife.cs
@@ -0,0 +1,44 @@
+﻿using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Decimation.Items
+{
+    public class SoulofLife : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Soul of Life");
+            Tooltip.SetDefault("The essence of organic beings.");
+            Main.RegisterItemAnimation(item.type, new DrawAnimationVertical(5, 4));
+            ItemID.Sets.AnimatesAsSoul[item.type] = true;
+            ItemID.Sets.ItemIconPulse[item.type] = true;
+            ItemID.Sets.ItemNoGravity[item.type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            Item refItem = new Item();
+            refItem.SetDefaults(ItemID.SoulofSight);
+            item.width = refItem.width;
+            item.height = refItem.height;
+            item.width = 20;
+            item.height = 20;
+            item.maxStack = 999;
+            item.value = 0;
+            item.rare = 1;
+        }
+
+        public class SoulGlobalNPC : GlobalNPC
+        {
+            public override void NPCLoot(NPC npc)
+            {
+                if (npc.type == NPCID.Plantera)
+                {
+                    Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("SoulofLife"), Main.rand.Next(12, 25));
+                }
+            }
+        }
+    }
+}
