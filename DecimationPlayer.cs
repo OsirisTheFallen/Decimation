645058798296005f0beecd25fac5e4be48c2325a
William <fylozdev@gmail.com>
2018-06-19 19:56:25 -0400

    Add files via upload

---
+++ DecimationPlayer.cs
@@ -0,0 +1,16 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Decimation
+{
+    public class DecimationPlayer : ModPlayer
+    {
+        public bool closeToEnchantedAnvil = false;
+
+        public override void ResetEffects()
+        {
+            //closeToEnchantedAnvil = false;
+        }
+    }
+}
