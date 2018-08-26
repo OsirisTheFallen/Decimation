e02d00332f85ef810b1ea7f5cb671db8bbb11b97
FyloZ <fylozdev@gmail.com>
2018-08-26 19:59:37 -0400

    Thermoplasm

--- DecimationWorld.cs
+++ DecimationWorld.cs
@@ -15,16 +15,19 @@ namespace Decimation
     {
 		public static bool downedBloodshotEye = false;	
 		public static bool downedDuneWorm = false;
+        public static bool downedArachnus = false;
 		public override void Initialize()
 		{
 			downedBloodshotEye = false;
 			downedDuneWorm = false;
+            downedArachnus = false;
 		}
 		public override TagCompound Save()
         {
             var downed = new List<string>();
             if (downedBloodshotEye) downed.Add("downedBloodshotEye");
 			if (downedDuneWorm) downed.Add("downedDuneWorm");
+            if (downedArachnus) downed.Add("downedArachnus");
             return new TagCompound	{
                 {"downed", downed},
 			};				
@@ -34,34 +37,40 @@ namespace Decimation
             var downed = tag.GetList<string>("downed");
             downedBloodshotEye = downed.Contains("downedBloodshotEye");
 			downedDuneWorm = downed.Contains("downedDuneWorm");
+            downedArachnus = downed.Contains("downedArachnus");
         }
 		public override void NetSend(BinaryWriter writer)
 		{
 			BitsByte flags = new BitsByte();
 			flags[0] = downedBloodshotEye; //+1 flag number for each new boss
 			flags[1] = downedDuneWorm;
+            flags[2] = downedArachnus;
 		}
 		public override void NetReceive(BinaryReader reader)
 		{
 			BitsByte flags = reader.ReadByte();
 			downedBloodshotEye = flags[0];
 			downedDuneWorm = flags[1];
+            downedArachnus = flags[2];
 		}
-		public override void LoadLegacy(BinaryReader reader)
+        public override void LoadLegacy(BinaryReader reader)
         {
             int loadVersion = reader.ReadInt32();
-            if(loadVersion == 1)
-            {         
-                byte flags=reader.ReadByte();
-                DecimationWorld.downedBloodshotEye=((flags&1)!=0);
-				DecimationWorld.downedDuneWorm=((flags&2)!=0); //double flag numbers with each new boss
+            if (loadVersion == 1)
+            {
+                byte flags = reader.ReadByte();
+                DecimationWorld.downedBloodshotEye = ((flags & 1) != 0);
+                DecimationWorld.downedDuneWorm = ((flags & 2) != 0); //double flag numbers with each new boss
+                DecimationWorld.downedArachnus = ((flags & 4) != 0);
             }
-            else if(loadVersion == 2)
-            {  
-                byte flags=reader.ReadByte();
-				byte flags2=reader.ReadByte();		
-                DecimationWorld.downedBloodshotEye=((flags&1)!=0);
-				DecimationWorld.downedDuneWorm=((flags&2)!=0);
-            }		
+            else if (loadVersion == 2)
+            {
+                byte flags = reader.ReadByte();
+                byte flags2 = reader.ReadByte();
+                DecimationWorld.downedBloodshotEye = ((flags & 1) != 0);
+                DecimationWorld.downedDuneWorm = ((flags & 2) != 0);
+                DecimationWorld.downedArachnus = ((flags & 4) != 0);
+            }
+        }
 	}
 }	
