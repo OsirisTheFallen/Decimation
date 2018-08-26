0c9c8568eaffe3d5837e99643f8445572fe4b27e
William <fylozdev@gmail.com>
2018-08-26 19:06:23 -0400

    Add files via upload

---
+++ Projectiles/ArchingSolarBlade.cs
@@ -0,0 +1,28 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Decimation.Projectiles
+{
+    class ArchingSolarBlade : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Arching Solar Blade");
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 42;       //projectile width
+            projectile.height = 46;  //projectile height
+            projectile.friendly = true;      //make that the projectile will not damage you
+            projectile.melee = true;         // 
+            projectile.tileCollide = false;   //make that the projectile will be destroed if it hits the terrain
+            projectile.penetrate = 999;      //how many npc will penetrate
+            projectile.timeLeft = 180;   //how many time this projectile has before disepire
+            projectile.extraUpdates = 1;
+            projectile.ignoreWater = true;
+            projectile.aiStyle = 3;
+        }
+    }
+}
---
+++ Projectiles/BlastofHeat.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Decimation.Projectiles
+{
+    class BlastofHeat : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Blast of Heat");
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 6;
+            projectile.height = 6;
+            projectile.aiStyle = 23;
+            projectile.hostile = true;
+            projectile.alpha = 255;
+            projectile.penetrate = -1;
+            projectile.extraUpdates = 3;
+            projectile.magic = true;
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            if (Main.rand.Next(2) == 0)
+                target.AddBuff(BuffID.OnFire, 600);
+            base.OnHitNPC(target, damage, knockback, crit);
+        }
+
+        public override void OnHitPlayer(Player target, int damage, bool crit)
+        {
+            if (Main.rand.Next(2) == 0)
+                target.AddBuff(BuffID.OnFire, 600);
+            base.OnHitPlayer(target, damage, crit);
+        }
+    }
+}
