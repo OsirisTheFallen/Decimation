4620938d4d6d689d0aff69428d7e912f89b68095
William <fylozdev@gmail.com>
2018-08-26 19:07:24 -0400

    Add files via upload

---
+++ Buffs/Hyperthermic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Decimation.Buffs
+{
+    class Hyperthermic : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            Main.debuff[Type] = true;
+            Main.buffNoSave[Type] = true;
+            Main.buffNoTimeDisplay[Type] = false;
+            DisplayName.SetDefault("Hyperthermic!");
+            Description.SetDefault("Water, water everywhere but not a drop to drink... \nBlock mana potions use \nLowers defense by 10% \nLowers speed by 5%");
+            canBeCleared = false;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.statDefense = (int)(player.statDefense * 0.90f);
+            player.moveSpeed *= 0.95f;
+        }
+    }
+
+    public class HyperthermicManaBlock : GlobalItem
+    {
+        public override bool CanUseItem(Item item, Player player)
+        {
+            if (player.HasBuff(mod.BuffType("Hyperthermic")) && item.healMana > 0)
+                return false;
+            else
+                return true;
+        }
+    }
+}
---
+++ Buffs/Singed.cs
@@ -0,0 +1,38 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Decimation.Buffs
+{
+    class Singed : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            Main.debuff[Type] = true;
+            Main.buffNoSave[Type] = true;
+            Main.buffNoTimeDisplay[Type] = false;
+            DisplayName.SetDefault("Singed!");
+            Description.SetDefault("Burn target \nSlow target by 5% \nLower target defense by 5% \n Block potion use");
+            canBeCleared = false;
+        }
+
+        public override void Update(NPC npc, ref int buffIndex)
+        {
+            npc.onFire = true;
+            npc.velocity -= (5 / 100) * npc.velocity;
+            npc.defense -= (5 / 100) * npc.defense;
+        }
+    }
+
+    public class SingedGlobalItem : GlobalItem
+    {
+        public override bool CanUseItem(Item item, Player player)
+        {
+            if (player.HasBuff(mod.BuffType("Singed")))
+            {
+                return !(item.UseSound != null && item.useStyle == 3);
+            }
+            return true;
+        }
+    }
+}
