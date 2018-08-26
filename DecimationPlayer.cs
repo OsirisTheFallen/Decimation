e5b75c00f0f319bf2f0e8f271383e7d2837025a1
William <fylozdev@gmail.com>
2018-08-26 19:07:47 -0400

    Add files via upload

--- DecimationPlayer.cs
+++ DecimationPlayer.cs
@@ -1,4 +1,5 @@
-﻿using Terraria;
+﻿using Microsoft.Xna.Framework;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -7,10 +8,19 @@ namespace Decimation
     public class DecimationPlayer : ModPlayer
     {
         public bool closeToEnchantedAnvil = false;
+        public bool isJestersQueverEquiped = false;
 
         public override void ResetEffects()
         {
-            //closeToEnchantedAnvil = false;
+            closeToEnchantedAnvil = false;
+            isJestersQueverEquiped = false;
+        }
+
+        public override bool Shoot(Item item, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            if (isJestersQueverEquiped && type == ProjectileID.WoodenArrowFriendly)
+                type = ProjectileID.JestersArrow;
+            return base.Shoot(item, ref position, ref speedX, ref speedY, ref type, ref damage, ref knockBack);
         }
     }
 }
