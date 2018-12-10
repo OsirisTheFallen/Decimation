1c456eed585d6dff737792ab2fb260465c7e8ed7
OsirisTheFallen <39780111+OsirisTheFallen@users.noreply.github.com>
2018-12-10 18:28:49 -0500

    Added TalonianPillar.cs and a temporary sprite

---
+++ Tiles/TalonianPillar.cs
@@ -0,0 +1,27 @@
+using System;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+
+namespace Decimation.Tiles
+{
+    class TalonianPillar : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileMergeDirt[Type] = true;
+            minPick = 200;
+            mineResist = 10;
+            dustType = DustID.Stone;
+            drop = mod.ItemType("TalonianPillar");
+            AddMapEntry(new Color(101, 195, 242));
+        }
+
+        public override void NumDust(int i, int j, bool fail, ref int num)
+        {
+            num = fail ? 1 : 3;
+        }
+    }
+}
