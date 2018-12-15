ef15c58bf1d981ffddc6b113702854ee074a529e
OsirisTheFallen <39780111+OsirisTheFallen@users.noreply.github.com>
2018-12-15 13:28:17 -0500

    added placeholder basalt tile/inventory sprite and their .cs files

---
+++ Tiles/Basalt.cs
@@ -0,0 +1,27 @@
+9using System;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+
+namespace Decimation.Tiles
+{
+    class Basalt : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileMergeDirt[Type] = true;
+            minPick = 20;
+            mineResist = 1;
+            dustType = DustID.Stone;
+            drop = mod.ItemType("Basalt");
+            AddMapEntry(new Color(192, 57, 85));
+        }
+
+        public override void NumDust(int i, int j, bool fail, ref int num)
+        {
+            num = fail ? 1 : 3;
+        }
+    }
+}
